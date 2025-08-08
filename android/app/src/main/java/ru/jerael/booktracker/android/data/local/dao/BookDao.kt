abcc506e1fb88e544c7b7a2a8f195273c6981608
Vladimir <l.kroyan43@gmail.com>
2025-08-08 19:51:12 +0300

    feat(BOOKTRACKER-3): Fetch and display books from backend

--- android/app/src/main/java/ru/jerael/booktracker/android/data/local/dao/BookDao.kt
+++ android/app/src/main/java/ru/jerael/booktracker/android/data/local/dao/BookDao.kt
@@ -2,6 +2,8 @@ package ru.jerael.booktracker.android.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 import ru.jerael.booktracker.android.data.local.entity.BookEntity
 
@@ -9,4 +11,16 @@ import ru.jerael.booktracker.android.data.local.entity.BookEntity
 interface BookDao {
     @Query("SELECT * FROM books")
     fun getAll(): Flow<List<BookEntity>>
+
+    @Transaction
+    suspend fun clearAndInsert(bookEntities: List<BookEntity>) {
+        clearAll()
+        upsertAll(bookEntities)
+    }
+
+    @Upsert
+    suspend fun upsertAll(booksEntities: List<BookEntity>)
+
+    @Query("DELETE FROM books")
+    suspend fun clearAll()
 }
--- android/app/src/main/java/ru/jerael/booktracker/android/presentation/ui/screens/book_list/BookListUiState.kt
+++ android/app/src/main/java/ru/jerael/booktracker/android/presentation/ui/screens/book_list/BookListUiState.kt
@@ -3,5 +3,6 @@ package ru.jerael.booktracker.android.presentation.ui.screens.book_list
 import ru.jerael.booktracker.android.domain.model.Book
 
 data class BookListUiState(
-    val books: List<Book> = emptyList()
+    val books: List<Book> = emptyList(),
+    val userMessage: String? = null
 )
