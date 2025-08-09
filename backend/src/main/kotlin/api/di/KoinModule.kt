fb51d3f1e0eb808b7ae847def82d9fe562ee24a3
Vladimir <l.kroyan43@gmail.com>
2025-08-09 21:51:38 +0300

    feat(BOOKTRACKER-6): Implement POST /books endpoint

--- backend/src/main/kotlin/api/di/KoinModule.kt
+++ backend/src/main/kotlin/api/di/KoinModule.kt
@@ -3,9 +3,11 @@ package ru.jerael.booktracker.backend.api.di
 import org.koin.dsl.module
 import ru.jerael.booktracker.backend.data.repository.BookRepositoryImpl
 import ru.jerael.booktracker.backend.domain.repository.BookRepository
+import ru.jerael.booktracker.backend.domain.usecases.AddBookUseCase
 import ru.jerael.booktracker.backend.domain.usecases.GetBooksUseCase
 
 val appModule = module {
     single<BookRepository> { BookRepositoryImpl() }
     single { GetBooksUseCase(get()) }
+    single { AddBookUseCase(get()) }
 }
--- backend/src/main/kotlin/api/plugins/Routing.kt
+++ backend/src/main/kotlin/api/plugins/Routing.kt
@@ -4,16 +4,24 @@ import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
-import ru.jerael.booktracker.backend.api.routes.bookRoutes
+import ru.jerael.booktracker.backend.api.routes.books
+import ru.jerael.booktracker.backend.domain.usecases.AddBookUseCase
 import ru.jerael.booktracker.backend.domain.usecases.GetBooksUseCase
 
 fun Application.configureRouting() {
     val getBooksUseCase: GetBooksUseCase by inject()
+    val addBookUseCase: AddBookUseCase by inject()
     val imageBaseUrl: String = dotenv()["STORAGE_BASE_URL"]
+    val coversPathPrefix: String = dotenv()["COVERS_PATH_PREFIX"]
 
     routing {
         route("/api") {
-            bookRoutes(getBooksUseCase, imageBaseUrl)
+            books(
+                getBooksUseCase = getBooksUseCase,
+                addBookUseCase = addBookUseCase,
+                imageBaseUrl = imageBaseUrl,
+                coversPathPrefix = coversPathPrefix
+            )
         }
     }
 }
